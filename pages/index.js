ea5c4340e0b2bb5e543efdb55c8630b70d005a23
Yoga723 <yogapangestu16@gmail.com>
2022-10-02 18:06:14 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ import Hoob from "../public/Hoob.png";
 import AboutImg from "../public/about me.png";
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <div
       className={`${darkMode === true ? `dark bg-[#111827]` : `bg-[#FBFAF5]`}`}
